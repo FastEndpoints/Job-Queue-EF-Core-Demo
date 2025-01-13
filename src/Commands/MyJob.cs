484964a894616012e84a3d767a6605a8234ae3c1
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2025-01-13 12:16:12 +0530

    add job progress tracking example

---
+++ src/Commands/MyJob.cs
@@ -0,0 +1,36 @@
+﻿namespace JobQueuesEfCoreDemo;
+
+sealed class MyJob : ITrackableJob<JobResult<MyEndResult>>
+{
+    public Guid TrackingID { get; set; }
+    public string Name { get; set; }
+}
+
+sealed class MyEndResult
+{
+    public string Message { get; set; }
+}
+
+sealed class MyJobHandler(IJobTracker<MyJob> tracker) : ICommandHandler<MyJob, JobResult<MyEndResult>>
+{
+    public async Task<JobResult<MyEndResult>> ExecuteAsync(MyJob job, CancellationToken ct)
+    {
+        var jobResult = new JobResult<MyEndResult>(totalSteps: 100)
+        {
+            CurrentStatus = "starting..."
+        };
+
+        for (var i = 0; i < 100; i++)
+        {
+            jobResult.CurrentStep = i;
+            jobResult.CurrentStatus = $"completed step: {i}";
+            await tracker.StoreJobResultAsync(job.TrackingID, jobResult, ct);
+            await Task.Delay(300);
+        }
+
+        jobResult.CurrentStatus = "all done!";
+        jobResult.Result = new() { Message = $"thank you {job.Name}!" };
+
+        return jobResult;
+    }
+}
--- src/Endpoints/Endpoints.cs
+++ src/Endpoints/Endpoints.cs
@@ -4,7 +4,7 @@ sealed class HelloEndpoint : EndpointWithoutRequest
 {
     public override void Configure()
     {
-        Post("hello/{firstName}/{lastName}");
+        Post("test/hello/{firstName}/{lastName}");
         AllowAnonymous();
     }
 
@@ -39,7 +39,7 @@ sealed class ResultEndpoint(IJobTracker<CommandC> jobTracker) : EndpointWithoutR
 {
     public override void Configure()
     {
-        Get("result/{trackingId}");
+        Get("test/result/{trackingId:guid}");
         AllowAnonymous();
     }
 
@@ -48,5 +48,55 @@ sealed class ResultEndpoint(IJobTracker<CommandC> jobTracker) : EndpointWithoutR
         var trackingId = Route<Guid>("trackingId");
         var result = await jobTracker.GetJobResultAsync<CommandCResult>(trackingId, c);
         Response = $"Your full name is: {result?.FullName}";
+    }
+}
+
+sealed class JobCreateEndpoint : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        Post("job/create/{yourName}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        var job = new MyJob { Name = Route<string>("yourName") ?? "john doe" };
+        var trackingId = await job.QueueJobAsync(ct: c);
+        await SendAsync($"Your job tracking Id is: {trackingId}");
+    }
+}
+
+sealed class JobProgressEndpoint : EndpointWithoutRequest<string>
+{
+    public override void Configure()
+    {
+        Post("job/progress/{trackingId:guid}");
+        AllowAnonymous();
+    }
+
+    public override async Task HandleAsync(CancellationToken c)
+    {
+        var trackingId = Route<Guid>("trackingId");
+        var jobResult = await JobTracker<MyJob>.GetJobResultAsync<JobResult<MyEndResult>>(trackingId, c);
+
+        if (jobResult is null)
+        {
+            await SendAsync("job execution hasn't begun yet!");
+
+            return;
+        }
+
+        switch (jobResult.IsComplete)
+        {
+            case false:
+                await SendAsync($"[{jobResult.CurrentStep} / {jobResult.TotalSteps}] | status: {jobResult.CurrentStatus}");
+
+                break;
+            case true:
+                await SendAsync($"end result: {jobResult.Result.Message}");
+
+                break;
+        }
     }
 }
